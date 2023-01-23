def3eec5e34eeedddd01124fe8216246a3012332
sodrew <phoebus8@hotmail.com>
2023-01-22 19:40:10 -0500

    adding attachment capability

--- send_notification.py
+++ send_notification.py
@@ -1,8 +1,9 @@
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 import smtplib
+import socket
+from email.message import EmailMessage
+from pathlib import Path
+
 import config
-import ssl
 
 class NotifyViaMail:
     def __init__(self):
@@ -14,50 +15,43 @@ class NotifyViaMail:
         try:
             self.smtp = smtplib.SMTP(config.server_smtp,
                                          config.port_smtp)
-            self.smtp.set_debuglevel(1)
-            self.smtp.ehlo('mylowercasehost')
+            # self.smtp.set_debuglevel(1)
+            lhost = socket.gethostname().lower()
+            self.smtp.ehlo(lhost)
             self.smtp.starttls()
-            self.smtp.ehlo('mylowercasehost')
+            self.smtp.ehlo(lhost)
             r, d = self.smtp.login(username, password, initial_response_ok=True)
-            # self.smtp.ehlo()
-            print("\tSMTP Signed in as %s" % self.username)
+            assert r == 235, 'login failed: %s' % str(r)
         except Exception as err:
             print("\tSign in error: %s" % str(err))
 
-    def send(self, recipient, subject, msg_text, msg_html=''):
-        # Instance of MIMEMultipart
-        msg = MIMEMultipart("alternative")
-
-        # Write the subject
-        msg["Subject"] = subject
-
+    def send(self, recipient, subject, message, filepath=''):
+        msg = EmailMessage()
         msg["From"] = self.username
         msg["To"] = recipient
+        msg["Subject"] = subject
+        msg.set_content(message)
 
-        # Attach the Plain body with the msg instance
-        msg.attach(MIMEText(msg_text, "plain"))
-
-        if(msg_html == ''):
-            msg_html = '<p>'+msg_text+'</p>'
-
-        # Attach the HTML body with the msg instance
-        msg.attach(MIMEText(msg_html, "html"))
+        if(filepath != '');
+            path = Path(filepath)
+            filename = path.name
+            msg.add_attachment(open(path, "r").read(), filename=filename)
 
         # Sending the mail
         self.smtp.sendmail(self.username, recipient, msg.as_string())
-        print('message to :{recipient} sent')
+        print(f'message to :{recipient} sent')
 
     def logout(self):
         self.smtp.quit()
 
 from email.message import EmailMessage
 def main():
-    # mail = NotifyViaMail()
-    # try:
-    #     mail.login(config.user, config.pwd)
-    #     mail.send(config.notify, 'hi', 'testing')
-    # finally:
-    #     mail.logout()
+    mail = NotifyViaMail()
+    try:
+        mail.login(config.user, config.pwd)
+        mail.send(config.notify, 'hi', 'testing', '~/desktop/outlook_junk_filter/README.md')
+    finally:
+        mail.logout()
 
     # sender = 'somename@outlook.com'
     # recipient = 'somename@gmail.com'
